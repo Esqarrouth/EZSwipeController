48d8cdb1088cf8704d98a9960fb501eea73394e8
Goktug Yilmaz <goktugyil@gmail.com>
2015-11-13 23:46:54 +0200

    Initial Commit

---
+++ EZSwipeController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EZSwipeController
+//
+//  Created by Goktug Yilmaz on 13/11/15.
+//  Copyright © 2015 Goktug Yilmaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
