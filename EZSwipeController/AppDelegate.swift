1f5a83ec6871d5ce79637764db260d1ab5981635
Mahen Mudalige <mahen3d@gmail.com>
2020-10-05 18:06:34 +1000

    Swift Version 5.2 upgraded

--- EZSwipeController/AppDelegate.swift
+++ EZSwipeController/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+    internal func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         // Override point for customization after application launch.
         window = UIWindow(frame: UIScreen.main.bounds)
         window!.rootViewController = MySwipeVC()
